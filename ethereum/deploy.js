a65e26c05106c9a851ae9c366f994beda7e2c7ac
Zubair Trabzada <zubair.trabzada@gmail.com>
2018-05-25 17:05:10 -0700

    testing and deployment complete

---
+++ ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider( // HDWalletProvider module connects to a target network and unlock a account by giving a neumonic code below
+    'gadget target scan planet uncover right extend picture spare lady silver veteran', 
+    'https://rinkeby.infura.io/Er9icAgAewb6EZTCdbXM' // infura connects to node to the eth network
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => { // so we can use await
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface)) //interface is ABI
+        .deploy({ data: compiledFactory.bytecode })
+        .send({ gas: '4000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);    
+};
+
+deploy();
--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -23,7 +23,7 @@ beforeEach(async () => {
         gas: '1000000'
     });
 
-    [campaignAddress] = await factory.mehtods.getDeployedCampaigns().call(); // this call returns the array of addresses from our deployed compaign (see solidity file for details)
+    [campaignAddress] = await factory.methods.getDeployedCampaigns().call(); // this call returns the array of addresses from our deployed compaign (see solidity file for details)
     // [compaignAdresses] same as compaignAddress = addresses[0]
     campaign = await new web3.eth.Contract(
         JSON.parse(compiledCampaign.interface),
@@ -31,9 +31,75 @@ beforeEach(async () => {
     );
 });
 
-describe('Campaigns', () => {
+describe('Campaigns', () => { // make sure both factory and campaign are successfully deployed by checking addresses on them
     it('deploys a factory and campaign', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
+    });
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call(); // retreive address of the manager
+        assert.equal(accounts[0], manager);
+    });
+
+    it('Allows people to contribute money and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributor);
+    });
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '5',
+                accounts: accounts[1]
+            });
+            assert(false); // it should spit out false since 5 is less than our min contribution of 100 wei    
+        } catch (err) {
+            assert(err);
+        }
+    });
+
+    it('allows a manager to make a payment request', async () => {
+        await campaign.methods
+            .createRequest('Buy batteries', '100', accounts[1]) // these params are from createRequest function from Campaign.sol
+            .send({
+                from: accounts[0],
+                gas: '1000000'
+            });
+        const request = await campaign.methods.requests(0).call();
+        
+        assert.equal('Buy batteries', request.description); // we dont need to check all the properties if one passes everything else should so lets just check the description property
+    });
+
+    it('processes requests', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei('10', 'ether')
+        });
+
+        await campaign.methods
+            .createRequest('adfa', web3.utils.toWei('5', 'ether'), accounts[1])
+            .send({ from: accounts[0], gas: '1000000' });
+
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[0], 
+            gas: '1000000'
+        });
+        
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        let balance = await web3.eth.getBalance(accounts[1]); // this returns a string
+        balance = web3.utils.fromWei(balance, 'ether');
+        balance = parseFloat(balance); // takes a string and turns it into a decimal number
+
+        //console.log(balance);
+        assert(balance > 104); // if that 5 ether got transfered from accounts[0] to accounts[1], it will pass
     });
 });
