3177f32bb5a582756d40ea9ba635e9f5788dbf56
Zubair Trabzada <zubair.trabzada@gmail.com>
2019-04-17 22:55:44 -0700

    testing routes

--- routes.js
+++ routes.js
@@ -1,11 +1,14 @@
-const routes = require('next-routes')(); // the second () means we are requiring a function and gets immediatly invoked
+const routes = require("next-routes")(); // the second () means we are requiring a function and gets immediatly invoked
 // see next-routes documentation for above and below codes examples
 
-
 routes
-    .add('/campaigns/new', '/campaigns/new')
-    .add('/campaigns/:address', '/campaigns/show') // :address is the wildcard or variable or placeholder that will be passed somewhere else
-    .add('/campaigns/:address/requests', '/campaigns/requests/index')
-    .add('/campaigns/:address/requests/new', '/campaigns/requests/new');
+  .add("/campaigns/new", "/campaigns/new")
+  .add("/campaigns/:address", "/campaigns/show") // :address is the wildcard or variable or placeholder that will be passed somewhere else
+  .add("/campaigns/:address/requests", "/campaigns/requests/index")
+  .add("/campaigns/:address/requests/new", "/campaigns/requests/new")
 
-    module.exports = routes;
+  // test routes
+  .add("/campaigns/:address/test", "/campaigns/tests/test")
+  .add("/campaigns/:address/test/new", "/campaigns/tests/new");
+
+module.exports = routes;
